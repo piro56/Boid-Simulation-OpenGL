0b93813221697a4ba47af353c0a501637e0f76b6
piro56 <meetvk57@gmail.com>
2021-09-21 00:27:18 -0400

    Added comments to all of the code. This is moreso for my own understanding and learning of OpenGL

--- src/Program.cpp
+++ src/Program.cpp
@@ -1,8 +1,58 @@
-#include <iostream>
+#include <iostream>     
+#include <glad/glad.h>  // Manages function pointers
+#include <glfw3.h>      // Manages window
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
 int main() {
-    std::cout << "Hello World!";
-    std::cin.get();
+    // Initialize and configure GLFW -> Set the version & 
+    // set profile to CORE so we do not get backwards-compatible features.
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Create window object.
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // Make 'window' the current context on the current thread.
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Window dimensions
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    // When window is resized -> call frambuffer_size_callback.
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop
+    while(!glfwWindowShouldClose(window))
+    {   
+        // Swapping buffers reduces artifacts.
+        glfwSwapBuffers(window);
+        // Polls events like keyboard/mouse inputs.
+        glfwPollEvents();    
+    }
+    
+    glfwTerminate();
     return 0;
 }
+
+// When user resizes window, resize the window in OpenGL to the new width and height
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}  
