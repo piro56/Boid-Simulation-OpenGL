12ccbec208e9b3534ada8dcd638e11f808b267c4
piro56 <meetvk57@gmail.com>
2021-10-03 00:44:24 -0400

    Modifying Textures

---
+++ src/headers/Texture.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <glad/glad.h>
+#include "stb_image.h"
+class Texture {
+private:
+    unsigned int ID;
+    bool loaded;
+    GLenum textureSlot;
+public:
+    // 
+    Texture(GLenum glTexture);
+
+    ~Texture();
+    inline unsigned int get_ID() {
+        return this->ID;
+    };
+
+    // Parameters
+    void setParameteri();
+    void setParameterfv();
+
+    // load png and JPG texture
+    // only load ONCE.
+    void load();        // generic load -> based off of extension ending...
+    void loadPNG();
+    void loadJPG();
+    // MANUAL LOADING
+    void load_image();
+    void tex_image_2D();
+
+    // Enabling and setting active.
+    void setActive();
+    void bindEnable();
+
+};
